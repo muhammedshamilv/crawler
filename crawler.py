796da95f8e1edd47e3337e534cf2ae68e67a89b0
shamil-v <shamil@hamon.in>
2022-03-18 10:42:54 +0530

    function for fetching songs and its link

--- crawler.py
+++ crawler.py
@@ -12,10 +12,24 @@ def get_artists(url):
         artist.append((i.text,i['href']))
     return artist
 
+
+def get_songs(artist_url):
+    songs=[]
+    r = requests.get(artist_url)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    tracklists = soup.find("table", {"class" : "tracklist"})
+    links=tracklists.find_all("a")
+    for i in links:
+        songs.append((i.text,i['href']))
+    return songs
+
+
 def crawl():
     artists= get_artists("https://www.songlyrics.com/a/")
     for name, link in artists:
         print(name, "   :   ",link)
+        songs = get_songs(link)
+        print(songs)
 
 if __name__ =="__main__":
     crawl()
